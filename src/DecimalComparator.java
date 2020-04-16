30f732c68be9447df4ce05c264dc7e17a43c8c9a
ryoray <praretno.wibowo@gmail.com>
2020-04-16 13:19:31 +0700

    Initial commit

---
+++ src/DecimalComparator.java
@@ -0,0 +1,10 @@
+public class DecimalComparator {
+
+    public static boolean areEqualByThreeDecimalPlaces(double firstNumber, double secondNumber) {
+
+        if (((int) (firstNumber * 1000)) == ((int) (secondNumber * 1000))) {
+            return true;
+        }
+        return false;
+    }
+}
